42e00341c07b748b3d925fc153909b4cc768340d
Marc Joliet <marcec@gmx.de>
2013-06-20 18:35:26 +0200

    Remove a comment.

--- start_qemu.sh
+++ start_qemu.sh
@@ -46,7 +46,6 @@ smp="sockets=1,cores=2,threads=1"
 # smp="2"
 boot="order=c,menu=off"
 machine="pc-1.0,accel=kvm,usb=off"
-# machine="pc-q35-1.4,accel=kvm,usb=off"
 layout="de"
 pc_definition="-name WindowsXP \
     -machine $machine \

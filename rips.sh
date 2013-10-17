cdf52d1a562a534a0809aca0a65179339d8b4cff
Marc Joliet <marcec@gmx.de>
2013-10-17 13:24:32 +0200

    rips.sh: automatically convert CUE sheets to UTF-8
    Cdda2wav apparently creates CUE sheets encoded as latin1, so tell split2flac to
    convert them to UTF-8 automatically.

--- rips.sh
+++ rips.sh
@@ -10,6 +10,9 @@ cdda2wav -paranoia -cddb 0 -t all -cuefile
 # set split2flac options
 out_pattern='@artist - {@year - }@album/@track - @title.@ext'
 flac_opts="-8V"
+# cdda2wav apparently by default creates latin1 encoded CUE sheets, and I can't
+# see how to change that
+split2flac_opts="-cue audio.cue -cuecharset latin1"
 
 # split the wav file into multiple tagged FLAC files
-split2flac.sh audio.wav -cue audio.cue -e "$flac_opts" -of "$out_pattern"
+split2flac.sh audio.wav $split2flac_opts -e "$flac_opts" -of "$out_pattern"

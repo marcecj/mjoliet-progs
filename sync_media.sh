e34fd2c8e7f20fbfad401ebf71d970a6236386c3
Marc Joliet <marcec@gmx.de>
2014-05-02 01:36:31 +0200

    Add a script to sync media to an external drive
    Add a script to synchronise my media directories to an external drive as a form
    of backup.

---
+++ sync_media.sh
@@ -0,0 +1,24 @@
+#!/bin/sh
+
+DIRS="/var/db/media
+/var/db/music"
+TARGET="/media/TOSHIBA EXT/marcec/"
+RSYNC_OPTIONS="-aX --delete --delete-excluded --numeric-ids"
+
+if [ ! -d "$TARGET" ];
+then
+    echo "Non-existent target!"
+    exit
+fi
+
+echo "$DIRS" | while read d;
+do
+    if [ ! -d "$d" ];
+    then
+        echo "Non-existent source!"
+        exit
+    fi
+
+    # echo rsync "$RSYNC_OPTIONS" "$d" "$TARGET"
+    rsync $RSYNC_OPTIONS "$d" "$TARGET"
+done

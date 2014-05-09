430da14327a839450b561b145c6f7aa8996f084a
Marc Joliet <marcec@gmx.de>
2014-05-09 02:11:22 +0200

    sync_media.sh: Exclude the lost+found directory
    I run sync_media.sh as a normal user, and the top-level lost+found/ directory
    is owned by root, so I cannot read it.  The problem is that when rsync fails
    due to the lack of permissions, it triggers an I/O error and rsync will refuse
    to delete any files (the --delete option).  Passing --exclude=lost+found/ fixes
    this.

--- sync_media.sh
+++ sync_media.sh
@@ -3,7 +3,7 @@
 DIRS="/var/db/media
 /var/db/music"
 TARGET="/media/TOSHIBA EXT/marcec/"
-RSYNC_OPTIONS="-aX --delete --delete-excluded --numeric-ids"
+RSYNC_OPTIONS="-aX --exclude=lost+found/ --delete --delete-excluded --numeric-ids"
 
 if [ ! -d "$TARGET" ];
 then

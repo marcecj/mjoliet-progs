b0cba7510d03626a54d5b629e1c606332cea95ef
Marc Joliet <marcec@gmx.de>
2014-10-27 21:40:34 +0100

    Update $TARGET to match new persistent mount point

--- sync_media.sh
+++ sync_media.sh
@@ -3,7 +3,7 @@
 DIRS="/home/marcec/Media
 /home/marcec/Music
 /home/marcec/Other"
-TARGET="/run/media/marcec/TOSHIBA EXT/marcec/"
+TARGET="/media/TOSHIBA EXT/marcec/"
 RSYNC_OPTIONS="-aX --exclude=lost+found/ --delete --delete-excluded --numeric-ids"
 
 if [ ! -d "$TARGET" ];

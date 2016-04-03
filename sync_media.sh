df3f50bad57c075929a876c35531a4121ee007cb
Marc Joliet <marcec@gmx.de>
2016-04-03 16:38:10 +0200

    sync_media.sh: add a "-r" option (for "remote")
    This allows one to specify a remote host as per rsync(1).

--- sync_media.sh
+++ sync_media.sh
@@ -6,21 +6,34 @@ DIRS="/home/marcec/Media
 TARGET="/media/MARCEC_BACKUP/Informal/"
 RSYNC_OPTIONS="-aX --exclude=lost+found/ --delete --delete-excluded --numeric-ids"
 PRETEND=0
+REMOTE=""
 
-while getopts p a
+while getopts pr: a
 do
     case $a in
         p) PRETEND=1;;
+        r) REMOTE=$OPTARG;;
         *) echo "Invalid option \"$a\"" >&2;
             exit;;
     esac
 done
 shift $(expr $OPTIND - 1)
 
-if [ ! -d "$TARGET" ];
+if [ -z "$REMOTE" -a ! -d "$TARGET" ]
 then
     echo "Non-existent target!" >&2
     exit
+fi
+
+if [ -n "$REMOTE" ]
+then
+    ssh "$REMOTE" exit
+    if [ $? != 0 ]; then
+        echo "Unable to connect to host" >&2
+        exit
+    fi
+
+    TARGET="$REMOTE:$TARGET"
 fi
 
 echo "Target is \"$TARGET\"."

ec0a24ef32e7df0c5499fa9ef877f82095e1ca5f
Marc Joliet <marcec@gmx.de>
2023-06-18 18:58:45 +0200

    sync_media.sh: rework to back up Music twice
    Rework sync_media.sh to back up Music twice: once as usual to the
    external backup HDD, then a second time to an internal file system to
    improve accesibilty from the media center PC.  It does this by changing
    $DIRS to be a list of source/target pairs and constructing the full
    target URL for each list item.

--- sync_media.sh
+++ sync_media.sh
@@ -1,8 +1,9 @@
 #!/bin/sh
 
-DIRS="/home/marcec/Media
-/home/marcec/Music"
-TARGET="/media/MARCEC_BACKUP/backups/Informal/"
+# a list of source/target pairs
+DIRS="/home/marcec/Media /media/MARCEC_BACKUP/backups/Informal/
+/home/marcec/Music /media/MARCEC_BACKUP/backups/Informal/
+/home/marcec/Music/ /mnt/barry_data/Music/"
 RSYNC_OPTIONS="-aX --one-file-system --exclude=lost+found/ --delete --delete-excluded --numeric-ids"
 PRETEND=0
 REMOTE=""
@@ -30,17 +31,18 @@ then
         echo "Unable to connect to host" >&2
         exit
     fi
-
-    TARGET="$REMOTE:$TARGET"
 fi
-
-echo "Target is \"$TARGET\"."
 
 [ $PRETEND -eq 1 ] && echo "Would execute:"
 
 echo "$DIRS" | while read -r d;
 do
-    if [ ! -d "$d" ];
+    source="$(echo $d | cut -d' ' -f1)"
+    target="$REMOTE:$(echo $d | cut -d' ' -f2)"
+
+    echo "Backing up \"$source\" to \"$target\"."
+
+    if [ ! -d "$source" ];
     then
         echo "Non-existent source!" >&2
         exit
@@ -48,8 +50,8 @@ do
 
     if [ $PRETEND -eq 1 ]
     then
-        echo rsync "$RSYNC_OPTIONS" "$d" "$TARGET"
+        echo rsync "$RSYNC_OPTIONS" "$source" "$target"
     else
-        rsync $RSYNC_OPTIONS "$d" "$TARGET"
+        rsync $RSYNC_OPTIONS "$source" "$target"
     fi
 done
